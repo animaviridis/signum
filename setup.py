b91e4394dcb050b6970d08d5cfeb7c949ac4d0a4
mysh <mysh995@gmail.com>
2021-05-11 16:18:51 +0200

    Version up

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="signum",
-    version="0.1.4",
+    version="0.1.5",
     author="savetheginger",
     author_email="mysh995@gmail.com",
     description="NumPy-based signal data container tools",
