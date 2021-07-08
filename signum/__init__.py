23d258f75c11ec91d41bfd61e8c7ca78f0cd9d3f
mysh <mysh995@gmail.com>
2021-07-08 10:23:17 +0200

    Corrected x axis spacing check for FFT

--- signum/__init__.py
+++ signum/__init__.py
@@ -2,4 +2,4 @@ from signum.core.containers import TimeDomainSignal, FreqDomainSignal
 from signum.tools.scale_manager import ScaleManager
 
 
-__version__ = '0.1.9'
+__version__ = '0.1.10'
--- signum/core/base_container.py
+++ signum/core/base_container.py
@@ -474,6 +474,9 @@ class SignalContainer(np.ndarray):
 
     def _check_x_axis_spacing(self, label='requested operation'):
         if self._nonstandard_x_axis is not None:
-            if np.unique(np.diff(self._nonstandard_x_axis)).size > 1:
+            u = np.unique(np.diff(self._nonstandard_x_axis)).size
+            m = np.median(u)
+
+            if (np.abs(u - m) > 10*np.finfo(u.dtype).resolution).any():
                 raise ValueError(f"Cannot perform {label} on an array of non-equally spaced samples")
 
