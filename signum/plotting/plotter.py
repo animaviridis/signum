f940981d20e7d6cb05b7b38994e0e0419403b79c
mysh <mysh995@gmail.com>
2021-10-13 11:01:24 +0200

    [Plotting] Refactoring

--- signum/plotting/plotter.py
+++ signum/plotting/plotter.py
@@ -4,14 +4,11 @@ import matplotlib.pyplot as plt
 
 class Plotter:
     def __init__(self, n_rows=1, n_cols=1, **kwargs):
-        fig_, axes_ = plt.subplots(n_rows, n_cols, **kwargs)
+        fig, axes = plt.subplots(n_rows, n_cols, **kwargs)
         if n_rows == 1 and n_cols == 1:
-            axes_ = np.array([axes_])
-        if axes_.ndim == 1:
-            axes_ = axes_.reshape(-1, 1)
+            axes = np.array([axes])
+        axes = axes.reshape(n_rows, n_cols)
 
-        self.fig = fig_
-        self.axes = axes_
-
-        self.plot_data = {}
+        self.fig = fig
+        self.axes = axes
 
