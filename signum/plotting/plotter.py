a6ce53ad40b92ef01ef51a5ac8ae61b6674c30bc
mysh <mysh995@gmail.com>
2022-03-29 10:21:22 +0200

    Units managed by ScaledAxes

--- signum/plotting/plotter.py
+++ signum/plotting/plotter.py
@@ -1,4 +1,3 @@
-from copy import deepcopy
 from typing import TYPE_CHECKING
 
 import matplotlib.pyplot as plt
@@ -7,7 +6,6 @@ import numpy as np
 if TYPE_CHECKING:
     from signum import SignalContainer
 
-from signum.tools.scale_manager import ScaleManager
 from signum.plotting.scaled_axes import ScaledAxes
 
 
@@ -27,28 +25,17 @@ class Plotter:
         self.lines = {}
         self.plot_data = {}
 
-        self.x_description = ''
-        self.y_description = ''
-
         if title:
             fig.suptitle(title)
 
         for ax in axes.flatten():
             ax.grid(color='lightgrey')
 
-    @property
-    def x_unit_bracket(self):
-        return f"[{self._x_unit}]" if self._x_unit else ''
-
-    @property
-    def unit_bracket(self):
-        return f"[{self._unit}]" if self._unit else ''
-
     def add_legend(self, **kwargs):
         """Add legend to each of the main grid axes."""
 
         for ax in self.axes.flatten():
-            ax.legend(fancybox=True, framealpha=0.5, **kwargs)
+            ax.add_legend(**kwargs)
 
     def show_fig(self, **kwargs):
         self.fig.show(**kwargs)
@@ -57,39 +44,7 @@ class Plotter:
     def show_all(**kwargs):
         plt.show(**kwargs)
 
-    def _fix_units(self, signal):
-        if self._unit and signal.unit and self._unit != signal.unit:
-            _, own_unit_core = ScaleManager.split_unit(self._unit)
-            _, signal_unit_core = ScaleManager.split_unit(signal.unit)
-            if own_unit_core != signal_unit_core:
-                raise ValueError(f"Signal unit: '{signal.unit}' does not match plot unit: '{self._unit}'")
-            order_change = ScaleManager.order_from_unit(self._unit) - ScaleManager.order_from_unit(signal.unit)
-            signal, new_unit = ScaleManager.rescale(signal, signal.unit, order_change)
-            signal.unit = new_unit
-
-        if self._x_unit and signal.x_unit and self._x_unit != signal.unit:
-            _, own_x_unit_core = ScaleManager.split_unit(self._x_unit)
-            _, signal_x_unit_core = ScaleManager.split_unit(signal.x_unit)
-            if own_x_unit_core != signal_x_unit_core:
-                raise ValueError(f"Signal x unit: '{signal.x_unit}' does not match plot x unit: '{self._x_unit}'")
-            order_change = ScaleManager.order_from_unit(self._x_unit) - ScaleManager.order_from_unit(signal.x_unit)
-            x_axis, new_unit = ScaleManager.rescale(signal.x_axis, signal.x_unit, order_change)
-            signal = deepcopy(signal)
-            signal.x_axis = x_axis
-            signal.x_unit = new_unit
-
-        self._unit = self._unit or signal.unit
-        self._x_unit = self._x_unit or signal.x_unit
-        self.x_description = self.x_description or signal.x_description
-
-        return signal
-
-    def _update_labels(self):
-        pass
-
     def add_line(self, signal: 'SignalContainer', add_legend=True, **kwargs):
-        signal = self._fix_units(signal)
-
         if signal.description and 'label' not in kwargs:
             kwargs['label'] = signal.description
 
@@ -102,8 +57,6 @@ class Plotter:
         self.lines[k] = lines
         self.plot_data[k] = signal
 
-        self._update_labels()
-
     def _add_line(self, signal, **kwargs):
         raise NotImplementedError
 
@@ -112,9 +65,8 @@ class SimplePlotter(Plotter):
     def __init__(self, x_label='', y_label='', **kwargs):
         super().__init__(n_rows=1, n_cols=1, **kwargs)
 
-        self.x_description = x_label
-        self.y_description = y_label
-        self._update_labels()
+        self.ax.set_xlabel(x_label)
+        self.ax.set_ylabel(y_label)
 
     @property
     def ax(self):
@@ -124,10 +76,6 @@ class SimplePlotter(Plotter):
         if np.iscomplexobj(signal):
             raise ValueError("Can't plot complex data on a SimplePlotter")
 
-        line, = self.ax.plot(signal.x_axis, signal, **kwargs)
+        line, = self.ax.plot(signal, **kwargs)
 
         return line,
-
-    def _update_labels(self):
-        self.ax.set_xlabel(f"{self.x_description} {self.x_unit_bracket}")
-        self.ax.set_ylabel(f"{self.y_description} {self.unit_bracket}")
--- signum/plotting/scaled_axes.py
+++ signum/plotting/scaled_axes.py
@@ -1,8 +1,13 @@
+import numpy as np
 import matplotlib.pyplot as plt
+import logging
 
 from signum.plotting.scaled_formatter import ScaledFormatter
 
+
 _NOT_GIVEN_ = object()
+
+logger = logging.getLogger(__name__)
 
 
 class ScaledAxes:
@@ -26,17 +31,25 @@ class ScaledAxes:
     def ax(self):
         return self._ax
 
+    @property
+    def x_ax_formatter(self):
+        return self.ax.xaxis.get_major_formatter()
+
+    @property
+    def y_ax_formatter(self):
+        return self.ax.yaxis.get_major_formatter()
+
     def get_x_unit(self):
-        return self.ax.xaxis.get_major_formatter().base_unit
+        return self.x_ax_formatter.base_unit
 
     def set_x_unit(self, x_unit):
-        self.ax.xaxis.get_major_formatter().base_unit = x_unit
+        self.x_ax_formatter.base_unit = x_unit
 
     def get_y_unit(self):
-        return self.ax.yaxis.get_major_formatter().base_unit
+        return self.y_ax_formatter.base_unit
 
     def set_y_unit(self, y_unit):
-        self.ax.yaxis.get_major_formatter().base_unit = y_unit
+        self.y_ax_formatter.base_unit = y_unit
 
     def get_units(self):
         return self.get_x_unit(), self.get_y_unit()
@@ -44,3 +57,46 @@ class ScaledAxes:
     def set_units(self, x_unit, y_unit):
         self.set_x_unit(x_unit)
         self.set_y_unit(y_unit)
+
+    def plot(self, *args, add_legend=True, **kwargs):
+        if len(args) == 2:
+            data, fmt = args
+        elif len(args) == 1:
+            data, = args
+            fmt = None
+        else:
+            raise TypeError(f"Expected 1 or 2 positional arguments, got {len(args)}")
+
+        try:
+            x_data = data.x_axis
+            y_data = data.view(np.ndarray)
+            x_unit = data.x_unit
+            y_unit = data.unit
+        except AttributeError:
+            raise TypeError(f"Expected a SignalContainer instance, got {type(data)}")
+
+        self._fix_units(x_unit, y_unit)
+
+        plot_args = (x_data, y_data, fmt) if fmt is not None else (x_data, y_data)
+        lines = self.ax.plot(*plot_args, **kwargs)
+
+        if add_legend and 'label' in kwargs:
+            self.add_legend()
+
+        return lines
+
+    def add_legend(self, fancybox=True, framealpha=0.5, **kwargs):
+        self.ax.legend(fancybox=fancybox, framealpha=framealpha, **kwargs)
+
+    def _fix_units(self, data_x_unit, data_y_unit):
+        data_units = {'x': data_x_unit, 'y': data_y_unit}
+
+        for xy in 'xy':
+            ax_unit = getattr(self, f'get_{xy}_unit')()
+            data_unit = data_units[xy]
+            if ax_unit:
+                if ax_unit != data_unit:  # TODO: orders of magnitude
+                    raise ValueError(f"{xy} units of the axes and data do not agree: {ax_unit=}, {data_unit=}")
+            else:
+                logger.debug(f"Setting {xy} unit of the axes to data {xy} unit: {data_unit}")
+                getattr(self, f'set_{xy}_unit', data_unit)
